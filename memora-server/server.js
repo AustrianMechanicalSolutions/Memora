3e6d97c87879db6b500a82ca37d2696433d2d8b0
Gland0rf <ndkogler@icloud.com>
2026-03-10 23:45:16 +0100

    fix angular serving path

--- memora-server/server.js
+++ memora-server/server.js
@@ -3,15 +3,23 @@ const path = require("path");
 
 const app = express();
 
-const distPath = path.join(__dirname, "../memora-frontend/dist/memora-frontend/browser")
+const distPath = path.resolve(__dirname, "../memora-frontend/dist/memora-frontend/browser");
+
+console.log("Serving Angular from:", distPath);
 
 app.use(express.static(distPath));
 
-app.get((req, res) => {
-    res.sendFile(path.join(distPath, "index.html"))
+app.get("/", (req, res) => {
+  res.sendFile(path.join(distPath, "index.html"));
+});
+
+// Angular routing fallback
+app.use((req, res) => {
+  res.sendFile(path.join(distPath, "index.html"));
 });
 
 const PORT = process.env.PORT || 8080;
+
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
