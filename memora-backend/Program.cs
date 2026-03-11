08db54ebcaf4a5828abede3a28cc53aa19d7dd57
Gland0rf <ndkogler@icloud.com>
2026-03-11 13:41:37 +0100

    Deleting the db over code

--- memora-backend/Program.cs
+++ memora-backend/Program.cs
@@ -81,11 +81,19 @@ app.MapGet("/api/me", (System.Security.Claims.ClaimsPrincipal user) =>
     });
 }).RequireAuthorization();
 
-using ( var scope = app.Services.CreateScope())
+using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
 
-    db.Database.EnsureDeleted();
+    var path = db.Database.GetDbConnection().DataSource;
+    Console.WriteLine("DB path: " + path);
+
+    if (File.Exists(path))
+    {
+        File.Delete(path);
+        Console.WriteLine("Deleted existing SQLite database");
+    }
+
     db.Database.Migrate();
 }
 
