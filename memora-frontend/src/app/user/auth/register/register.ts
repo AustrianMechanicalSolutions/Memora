b5ab09e7ee12c3b71c043e6d24e31315cf4df520
Gland0rf <ndkogler@icloud.com>
2026-05-11 00:49:01 +0200

    Set up 2FA again. Also added backup-codes, login-credentials, etc.

--- memora-frontend/src/app/user/auth/register/register.ts
+++ memora-frontend/src/app/user/auth/register/register.ts
@@ -4,14 +4,8 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
-<<<<<<< HEAD
-import { TranslatePipe } from '../../../translate.pipe';
-import { I18nService } from '../../../i18n.service';
-import { HttpErrorResponse } from '@angular/common/http';
-=======
 import { TranslatePipe } from '../../../translation/translate.pipe';
 import { I18nService } from '../../../translation/i18n.service';
->>>>>>> origin/main
 
 @Component({
   standalone: true,
@@ -36,10 +30,6 @@ export class RegisterComponent {
 
     this.auth.register(this.email, this.password).subscribe({
       next: () => this.router.navigate(['/']),
-<<<<<<< HEAD
-      error: (err: HttpErrorResponse) => {
-        this.error = err.error?.message ?? this.i18n.translate('auth.register.failed')
-=======
       error: (e) => {
         const status = e?.status;
         const err = e?.error?.error;
@@ -65,7 +55,6 @@ export class RegisterComponent {
 
         // Fallback
         this.errorMsg = this.i18n.translate('auth.register.failed');
->>>>>>> origin/main
       }
     });
   }
