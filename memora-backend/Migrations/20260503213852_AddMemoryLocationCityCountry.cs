d9cb1bcec4ae0bce90e4d713e43a0072fd08c223
Glandorf <63344328+Gland0rf@users.noreply.github.com>
2026-05-10 22:59:34 +0200

    Feature/albums (#36)
    * Reverted
    
    * Reversed
    
    * Created backend
    
    * Added login and register sites
    
    * Created the db with a user, an auth dto and endpoints for auth
    
    * Integrated auth backend into the frontend
    
    * Refined login / logout features, added user settings to enable 2fa auth
    
    * Remove build artifacts from repo and add .gitignore in memora_backend
    
    * Fixed merge conflicts
    
    * Renamed GroupDtos to GroupDTOs
    
    * Updated routes and group colors
    
    * Visual changes | videos and pictures are now stored in the database
    
    * Removed wwwroot
    
    * added a proxy setting
    
    * Added a members tab to view members, also added backend endpoint for membes list
    
    * Added albums site
    
    * Removed migrations
    
    * Removed stupid shit
    
    * Albums are now creatable, viewable, and are able to store memories in. There also is a 'all' memories album
    
    * Idk I got an error so I will commit
    
    * Updated groups view for nicer view
    
    * Updated the groups view with some real data
    
    * Fixed 1 hour offset between front- and backend by adding a time-gurard | Removed more placeholders in group detail page
    
    * Updated group detail page to be 100% real values and no placeholders anymore
    
    * error: Your local changes to the following files would be overwritten by checkout
    
    * Fixed another merge conflict
    
    * Improved the albums design by a lot
    
    * Made it possible to add and remove people from albums (as people that 'were there')
    
    * Added stuff
    
    * Git error or sth I have to commit
    
    * added settings
    
    * Fixed videos not loading
    
    * Added database to gitignore so everyone can have their own local db
    
    * Up to date with main
    
    * Updated the backend to also save people and the location of memories
    
    * Updated frontend to support location with gps
    
    * Adding people works now too in the frontend
    
    * Fixed a user stats error
    
    * Updated memories to have a title too
    
    * Changes
    
    * Added a search bar, letting you search for title and location for now. Levitathan function is implemented, tokenization is used.
    
    * Added a backend search endpoint for names
    
    * Added testing page
    
    * Added testing site and fixed backend return value
    
    * Fixed back and frontend to actually work
    
    * Names are now searchable too, but not using the backend names dataset anymore since it is obsolete
    
    * Comitting changes
    
    * Udpated gitignore
    
    * Commiting migrations now
    
    * Started working on an auto fill for countries and cities
    
    * Lat und Long are automatically converted to country and city via a dataset. This is shown and is also used if you search for a memory.
    
    * Fixed error
    
    * Removed dataset from gitignore
    
    * Change migrations
    
    * Remove obj cache from tracking
    
    * Added databases again, fixed migrations

--- memora-backend/Migrations/20260503213852_AddMemoryLocationCityCountry.cs
+++ memora-backend/Migrations/20260503213852_AddMemoryLocationCityCountry.cs
@@ -8,15 +8,30 @@ namespace memorabackend.Migrations
     public partial class AddMemoryLocationCityCountry : Migration
     {
         /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
+       protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.AddColumn<string>(
+                name: "LocationCity",
+                table: "Memory",
+                type: "TEXT",
+                nullable: true);
 
+            migrationBuilder.AddColumn<string>(
+                name: "LocationCountry",
+                table: "Memory",
+                type: "TEXT",
+                nullable: true);
         }
 
-        /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.DropColumn(
+                name: "LocationCity",
+                table: "Memory");
 
+            migrationBuilder.DropColumn(
+                name: "LocationCountry",
+                table: "Memory");
         }
     }
 }
